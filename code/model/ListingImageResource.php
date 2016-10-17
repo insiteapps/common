2d4fe54f9588381e0905bf592db425ab885a7596
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-10-17 06:53:19 +0200

    Image resource

---
+++ code/model/ListingImageResource.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Class ListingImageResource
+ */
+class ListingImageResource extends DataObject
+{
+
+    private static $default_sort = 'SortOrder';
+    private static $db = array(
+        'Name' => 'Varchar(255)',
+        'Description' => 'Text',
+        'SortOrder' => 'Int',
+    );
+    private static $has_one = array(
+        'Image' => 'FittedImage',
+        'Page' => 'Page',
+    );
+
+    public function getCMSFields()
+    {
+        $f = parent::getCMSFields();
+        $f->removeByName('SortOrder');
+        $f->removeByName('PageID');
+
+        return $f;
+    }
+
+    private static $summary_fields = array(
+        'Thumbnail',
+        'Name',
+        'Description'
+    );
+
+    function getThumbnail()
+    {
+        $image = $this->Image();
+        if ($image && $image->ID) {
+            return $image->CMSThumbnail();
+        }
+    }
+
+}
