ed495897508c88e08cfdb3e146a4c7720dcbd67a
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-10-22 13:44:53 +0200

    code update

--- code/managers/InsiteAppsListingManager.php
+++ code/managers/InsiteAppsListingManager.php
@@ -5,6 +5,17 @@
  */
 class InsiteAppsListingManager extends InsiteAppsCommonManager
 {
+    public static function getPropertyMinMax($field)
+    {
+        $oProperties = PropertyPage::get();
+        $data = $oProperties->column($field);
+        $data = array_unique($data);
+        asort($data);
+        $min = min($data);
+        $max = max($data);
+        return array_combine(range($min, $max), range($min, $max));
+
+    }
 
     public static function get_listing_area_map()
     {
