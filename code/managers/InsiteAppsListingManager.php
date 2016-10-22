8a67bdf1b9b014cc1cc61fcf2cbf62127073ef28
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-10-22 15:27:25 +0200

    code update

--- code/managers/InsiteAppsListingManager.php
+++ code/managers/InsiteAppsListingManager.php
@@ -17,36 +17,13 @@ class InsiteAppsListingManager extends InsiteAppsCommonManager
 
     }
 
-    public static function get_listing_area_map()
-    {
-        $oListingArea = ListingArea::get();
-        $oListingAreaMap = $oListingArea ? $oListingArea->map()->toArray() : array();
-        asort($oListingAreaMap);
-        return $oListingAreaMap;
-    }
 
-    public static function get_listing_location_map()
+    public static function get_listing_object_map($className, $key = "ID")
     {
-        $oListingLocation = ListingLocation::get();
-        $oListingLocationMap = $oListingLocation ? $oListingLocation->map()->toArray() : array();
-        asort($oListingLocationMap);
-        return $oListingLocationMap;
-    }
-
-    public static function get_listing_type_map()
-    {
-        $oListingType = ListingType::get();
-        $oListingTypeMap = $oListingType ? $oListingType->map()->toArray() : array();
-        asort($oListingTypeMap);;
-        return $oListingTypeMap;
-    }
-
-    public static function get_listing_collection_map()
-    {
-        $oListingCollection = ListingCollection::get();
-        $oListingCollectionMap = $oListingCollection ? $oListingCollection->map()->toArray() : array();
-        asort($oListingCollectionMap);
-        return $oListingCollectionMap;
+        $oObj = $className::get();
+        $oObjMap = $oObj ? $oObj->map($key, "Title")->toArray() : array();
+        asort($oObjMap);
+        return $oObjMap;
     }
 
 }
