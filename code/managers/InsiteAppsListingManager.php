b10679f0feb4a4346bbd895cd78f5f2f0807db87
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-10-22 09:43:40 +0200

    code update

--- code/managers/InsiteAppsListingManager.php
+++ code/managers/InsiteAppsListingManager.php
@@ -14,4 +14,28 @@ class InsiteAppsListingManager extends InsiteAppsCommonManager
         return $oListingAreaMap;
     }
 
+    public static function get_listing_location_map()
+    {
+        $oListingLocation = ListingLocation::get();
+        $oListingLocationMap = $oListingLocation ? $oListingLocation->map()->toArray() : array();
+        asort($oListingLocationMap);
+        return $oListingLocationMap;
+    }
+
+    public static function get_listing_type_map()
+    {
+        $oListingType = ListingType::get();
+        $oListingTypeMap = $oListingType ? $oListingType->map()->toArray() : array();
+        asort($oListingTypeMap);;
+        return $oListingTypeMap;
+    }
+
+    public static function get_listing_collection_map()
+    {
+        $oListingCollection = ListingCollection::get();
+        $oListingCollectionMap = $oListingCollection ? $oListingCollection->map()->toArray() : array();
+        asort($oListingCollectionMap);
+        return $oListingCollectionMap;
+    }
+
 }
