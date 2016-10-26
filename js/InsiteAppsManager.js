8fa75fba45d9823fb0992f4f622b2a12e9aef21d
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-10-26 11:51:00 +0200

    code update

--- js/InsiteAppsManager.js
+++ js/InsiteAppsManager.js
@@ -1,43 +1,43 @@
+/**
+ * Shared variables
+ */
+var ua = navigator.userAgent.toLowerCase(),
+    platform = navigator.platform.toLowerCase(),
+    $window = $(window),
+    $document = $(document),
+    $html = $('html'),
+    $body = $('body'),
+
+    android_ancient = (ua.indexOf('mozilla/5.0') !== -1 && ua.indexOf('android') !== -1 && ua.indexOf('applewebKit') !== -1) && ua.indexOf('chrome') === -1,
+    apple = ua.match(/(iPad|iPhone|iPod|Macintosh)/i),
+    webkit = ua.indexOf('webkit') != -1,
+
+    isiPhone = false,
+    isiPod = false,
+    isAndroidPhone = false,
+    android = false,
+    iOS = false,
+    isIE = false,
+    ieMobile = false,
+    isSafari = false,
+    isMac = false,
+    isWindows = false,
+    isiele10 = false,
+
+    firefox = ua.indexOf('gecko') != -1,
+    safari = ua.indexOf('safari') != -1 && ua.indexOf('chrome') == -1,
+
+    is_small = $('.js-nav-trigger').is(':visible'),
+
+    windowHeight = $window.height(),
+    windowWidth = $window.width(),
+    documentHeight = $document.height(),
+    orientation = windowWidth > windowHeight ? 'portrait' : 'landscape';
+
+
 (function ($) {
     "use strict";
     /*global jQuery, document, window*/
-
-    /**
-     * Shared variables
-     */
-    var ua = navigator.userAgent.toLowerCase(),
-        platform = navigator.platform.toLowerCase(),
-        $window = $(window),
-        $document = $(document),
-        $html = $('html'),
-        $body = $('body'),
-
-        android_ancient = (ua.indexOf('mozilla/5.0') !== -1 && ua.indexOf('android') !== -1 && ua.indexOf('applewebKit') !== -1) && ua.indexOf('chrome') === -1,
-        apple = ua.match(/(iPad|iPhone|iPod|Macintosh)/i),
-        webkit = ua.indexOf('webkit') != -1,
-
-        isiPhone = false,
-        isiPod = false,
-        isAndroidPhone = false,
-        android = false,
-        iOS = false,
-        isIE = false,
-        ieMobile = false,
-        isSafari = false,
-        isMac = false,
-        isWindows = false,
-        isiele10 = false,
-
-        firefox = ua.indexOf('gecko') != -1,
-        safari = ua.indexOf('safari') != -1 && ua.indexOf('chrome') == -1,
-
-        is_small = $('.js-nav-trigger').is(':visible'),
-
-        windowHeight = $window.height(),
-        windowWidth = $window.width(),
-        documentHeight = $document.height(),
-        orientation = windowWidth > windowHeight ? 'portrait' : 'landscape';
-
 
     jQuery(document).ready(function () {
         var InsiteAppsManagerInstance = new InsiteAppsManager();
