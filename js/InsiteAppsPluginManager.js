975534e5e9722cb5eb1f537e4f89e589ad4266ee
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-11-01 15:53:39 +0200

    adding hammer js

--- js/InsiteAppsPluginManager.js
+++ js/InsiteAppsPluginManager.js
@@ -140,3 +140,85 @@ function InsiteAppsPluginManager() {
         }
     });
 })(jQuery);
+
+
+/*
+ * debouncedresize: special jQuery event that happens once after a window resize
+ *
+ * latest version and complete README available on Github:
+ * https://github.com/louisremi/jquery-smartresize
+ *
+ * Copyright 2012 @louis_remi
+ * Licensed under the MIT license.
+ *
+ * This saved you an hour of work?
+ * Send me music http://www.amazon.co.uk/wishlist/HNTU0468LQON
+ */
+(function($) {
+
+    var $event = $.event,
+        $special,
+        resizeTimeout;
+
+    $special = $event.special.debouncedresize = {
+        setup: function() {
+            $(this).on("resize", $special.handler);
+        },
+        teardown: function() {
+            $(this).off("resize", $special.handler);
+        },
+        handler: function(event, execAsap) {
+            // Save the context
+            var context = this,
+                args = arguments,
+                dispatch = function() {
+                    // set correct event type
+                    event.type = "debouncedresize";
+                    $event.dispatch.apply(context, args);
+                };
+
+            if (resizeTimeout) {
+                clearTimeout(resizeTimeout);
+            }
+
+            execAsap ?
+                dispatch() :
+                resizeTimeout = setTimeout(dispatch, $special.threshold);
+        },
+        threshold: 150
+    };
+
+})(jQuery);
+
+(function($) {
+    $.fn.extend({
+        getColumnsWidth: function() {
+
+            // append an empty <span>
+            $this = $(this).append('<span></span>');
+
+            // grab left position
+            var pos = $this.find('span:last-of-type').position().left;
+
+            // get prefix for css3
+            var prefix;
+            if (jQuery.browser.webkit) prefix = '-webkit-';
+            else if (jQuery.browser.opera) prefix = '-o-';
+            else if (jQuery.browser.mozilla) prefix = '-moz-';
+            else if (jQuery.browser.msie) prefix = '-ms-';
+
+            // add the width of the final column
+            pos += parseInt($this.css(prefix + 'column-width'), 10);
+
+            // subtract one column gap (not sure why this is necessary?)
+            pos -= parseInt($this.css(prefix + 'column-gap'), 10);
+
+            // remove empty <span>
+            $(this).find('span:last-of-type').remove();
+
+            // return position
+            return pos;
+
+        }
+    });
+})(jQuery);
