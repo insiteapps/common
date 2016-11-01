07938ba543c3cc6ee80fb46f5aac2c67757d9077
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-11-01 15:34:00 +0200

    adding hammer js

--- js/InsiteAppsManager.js
+++ js/InsiteAppsManager.js
@@ -40,117 +40,59 @@ window.$html = $('html'), window.$body = $('body');
     /*global jQuery, document, window*/
 
     jQuery(document).ready(function () {
-        var InsiteAppsManagerInstance = new InsiteAppsManager();
+        InsiteAppsManager.init();
     });
 
     $window.on('resize', function () {
-        var InsiteAppsManagerInstance = new InsiteAppsManager();
+        //var InsiteAppsManagerInstance = new InsiteAppsManager();
 
     });
-    var InsiteAppsManager = function () {
-        var self = this;
-        $.proxy(self.init, self);
-        self.init();
-        self.browserSize();
-        self.browserSupport();
-
-    };
-    InsiteAppsManager.prototype.init = function () {
-
-    };
-
-    InsiteAppsManager.prototype.browserSize = function () {
-        window.windowHeight = $window.height();
-        window.windowWidth = $window.width();
-        window.documentHeight = $document.height();
-        window.orientation = windowWidth > windowHeight ? 'portrait' : 'landscape';
-    }
-    InsiteAppsManager.prototype.browserSupport = function () {
-        $.support.touch = Modernizr.touchevents;
-        $.support.svg = (document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")) ? true : false;
-        $.support.transform = getSupportedTransform();
-
-        $html
-            .addClass($.support.touch ? 'touch' : 'no-touch')
-            .addClass($.support.svg ? 'svg' : 'no-svg')
-            .addClass(!!$.support.transform ? 'transform' : 'no-transform');
-    }
-    function getSupportedTransform() {
-        var prefixes = ['transform', 'WebkitTransform', 'MozTransform', 'OTransform', 'msTransform'];
-        for (var i = 0; i < prefixes.length; i++) {
-            if (document.createElement('div').style[prefixes[i]] !== undefined) {
-                return prefixes[i];
-            }
-        }
-        return false;
-    }
-    /*
-
-     var Placeholder = (function() {
-     var $items;
-
-     function update($container, src) {
-
-     var $container = $container || $('body');
-
-     $items = $container.find('.insite-js-placeholder');
-
-     $items.each(function(i, item) {
-     var $item = $(item);
-     $item.data('actualHeight', $item.height());
-     });
-
-     $items.each(function(i, item) {
-     var $item = $(item).data('loaded', false),
-     width = $item.data('width'),
-     height = $item.data('height'),
-     newHeight = $item.height(),
-     newWidth = Math.round(newHeight * $item.data('width') / $item.data('height')),
-     $image = $(document.createElement('img')).css('opacity', 0);
-
-     $item.toggleClass('is--portrait', height > width);
-
-     $item.width(newWidth);
-     $item.data('image', $image);
-     });
-
-     $(window).on('DOMContentLoaded load resize scroll djaxLoad', bindImageLoad);
-     $('.portfolio-grid, .site-content').on('scroll', bindImageLoad);
-
-     bindImageLoad();
-
-     $(window).on('djaxClick', function() {
-     $(window).off('DOMContentLoaded load resize scroll djaxLoad', bindImageLoad);
-     $('.portfolio--grid, .site-content').off('scroll', bindImageLoad);
-     });
-     }
-
-
-
-     function onResize() {
-     $items.each(function(i, item) {
-     var $item = $(item),
-     width = $item.data('width'),
-     height = $item.data('height'),
-     newHeight = $item.height(),
-     newWidth = Math.round(newHeight * width / height);
-
-     $item.data('newWidth', newWidth);
-     });
-
-     $items.each(function(i, item) {
-     var $item = $(item);
-     $item.width($item.data('newWidth'));
-     });
-     }
-
-     return {
-     update: update,
-     resize: onResize
-     }
-
-     })();
-     */
-
-
 }(jQuery));
+
+
+var InsiteAppsManager = function () {
+    var self = this;
+
+
+    return {
+        init: function () {
+            $.proxy(self.init, self);
+            self.init();
+            self.browserSize();
+            self.browserSupport();
+        }
+    }
+
+}();
+InsiteAppsManager.init = function () {
+
+};
+
+InsiteAppsManager.browserSize = function () {
+    window.windowHeight = $window.height();
+    window.windowWidth = $window.width();
+    window.documentHeight = $document.height();
+    window.orientation = windowWidth > windowHeight ? 'portrait' : 'landscape';
+}
+InsiteAppsManager.browserSupport = function () {
+    $.support.touch = Modernizr.touchevents;
+    $.support.svg = (document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")) ? true : false;
+    $.support.transform = getSupportedTransform();
+
+    $html
+        .addClass($.support.touch ? 'touch' : 'no-touch')
+        .addClass($.support.svg ? 'svg' : 'no-svg')
+        .addClass(!!$.support.transform ? 'transform' : 'no-transform');
+}
+function getSupportedTransform() {
+    var prefixes = ['transform', 'WebkitTransform', 'MozTransform', 'OTransform', 'msTransform'];
+    for (var i = 0; i < prefixes.length; i++) {
+        if (document.createElement('div').style[prefixes[i]] !== undefined) {
+            return prefixes[i];
+        }
+    }
+    return false;
+}
+
+
+
