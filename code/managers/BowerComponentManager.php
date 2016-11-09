57af9e12508ab52fe6e1d91927f3f13bb2167172
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-11-09 10:34:09 +0200

    code update

--- code/managers/BowerComponentManager.php
+++ code/managers/BowerComponentManager.php
@@ -21,7 +21,7 @@ class BowerComponentManager extends Object
 
             BOWER_COMPONENTS_DIR . '/wow/dist/wow.min.js',
 
-            INSITE_COMMON_DIR . '/js/modernizr.custom.js',
+            INSITE_COMMON_DIR . '/js/modernizr.min.js',
 
             BOWER_COMPONENTS_DIR . '/isotope-horizontal/horizontal.js',
             BOWER_COMPONENTS_DIR . '/isotope-masonry-horizontal/masonry-horizontal.js',
