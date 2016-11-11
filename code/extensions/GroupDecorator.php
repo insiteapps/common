265382795428bcf3a7fd7e16d617eba1a847b7de
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-11-11 11:21:57 +0200

    code update

--- code/extensions/GroupDecorator.php
+++ code/extensions/GroupDecorator.php
@@ -4,9 +4,6 @@ class GroupDecorator extends DataExtension
 {
 
     static $db = array(
-        "MemberGroup" => "Boolean",
-        "GroupLevel" => "Int",
-        "CanSignUp" => "Boolean",
         "GoToAdmin" => "Boolean",
     );
     static $has_one = array(
@@ -15,9 +12,6 @@ class GroupDecorator extends DataExtension
 
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->addFieldToTab("Root.Members", new CheckboxField("MemberGroup", "Member Group"), 'Members');
-        $fields->addFieldToTab("Root.Members", new NumericField("GroupLevel", "Group Level"), 'Members');
-        $fields->addFieldToTab("Root.Members", new CheckboxField("CanSignUp", " Can Signup"), 'Members');
         $fields->addFieldToTab("Root.Members", new CheckboxField("GoToAdmin", " Go to Admin area"), 'Members');
         $fields->addFieldToTab("Root.Members", new TreeDropdownField("LinkPageID", "Or select a Page to redirect to", "SiteTree"), 'Members');
     }
