79599a70575fa640e52ea8867b9dac8c4c8cb7f9
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-12-03 10:45:11 +0200

    code update

---
+++ code/managers/DatabaseQueryTimer.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DatabaseQueryTimer
+ *
+ * @author User
+ */
+class DatabaseQueryTimer {
+
+    private $timer;
+
+    public function __construct() {
+        $this->timer = microtime(true);
+    }
+
+    function StartTimer($what = '') {
+        $this->timer = 0; //global variable to store time       
+        echo '<p style="border:1px solid black; color: black; background: yellow;">';
+        echo " Running <i>$what</i>. ";
+        flush(); //output this to the browser       
+        list ($usec, $sec) = explode(' ', microtime());
+        $this->timer = ((float) $usec + (float) $sec); //set the timer
+    }
+
+    function StopTimer() {
+        if ($this->timer > 0) {
+            list ($usec, $sec) = explode(' ', microtime()); //get the current time
+            $this->timer = ((float) $usec + (float) $sec) - $this->timer; //the time taken in milliseconds
+            echo ' Took ' . number_format($this->timer, 4) . ' seconds.</p>';
+            flush();
+        }
+    }
+
+}
