b321c09f9cc11956b88da1aabeae517d09942312
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2017-05-23 15:14:49 +0200

    batch code update - common

--- src/managers/BowerComponentManager.php
+++ src/managers/BowerComponentManager.php
@@ -12,7 +12,7 @@ class BowerComponentManager extends Object
     {
         self::includeBootstrap($aRequirements);
         $aRequirements["JS"] = array_merge($aRequirements["JS"], [
-            BOWER_COMPONENTS_DIR . '/jquery-migrate/jquery-migrate.js',
+            //BOWER_COMPONENTS_DIR . '/jquery-migrate/jquery-migrate.js',
             JQUERY_UI_DIR . '/jquery-ui.min.js',
             JQUERY_FORM_DIR . '/jquery.form.min.js',
             INSITE_COMMON_DIR . "/js/js.cookie.js",
