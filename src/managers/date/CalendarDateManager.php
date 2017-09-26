be9a8ef9fe8b92f1891383014911e0559a050a46
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2017-09-26 10:06:47 +0200

    batch code update - common

--- src/managers/date/CalendarDateManager.php
+++ src/managers/date/CalendarDateManager.php
@@ -1,30 +1,31 @@
 <?php
+
 /**
  *
  * Copyright (c) 2017 Insite Apps - http://www.insiteapps.co.za
  * All rights reserved.
+ *
  * @package insiteapps
- * @author Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * @author  Patrick Chitovoro  <patrick@insiteapps.co.za>
  * Redistribution and use in source and binary forms, with or without modification, are NOT permitted at all.
  * There is no freedom to share or change it this file.
  *
  *
  */
-
 class CalendarDateManager extends CalendarTimeManager
 {
-
-
+    
+    
     /**
      * The timestamp for this sfDate instance.
      */
     private $ts = null;
-
+    
     /**
      * The original timestamp for this sfDate instance.
      */
     private $init = null;
-
+    
     /**
      * Retrieves a new instance of this class.
      *
@@ -36,13 +37,14 @@ class CalendarDateManager extends CalendarTimeManager
      * </code>
      *
      * @param    mixed    timestamp, string, or sfDate object
+     *
      * @return    sfDate
      */
     public static function getInstance($value = null)
     {
         return new self($value);
     }
-
+    
     /**
      * Construct an sfDate object.
      *
@@ -52,7 +54,7 @@ class CalendarDateManager extends CalendarTimeManager
     {
         $this->set($value);
     }
-
+    
     /**
      * Format the date according to the <code>date</code> function.
      *
@@ -62,7 +64,7 @@ class CalendarDateManager extends CalendarTimeManager
     {
         return date($format, $this->ts);
     }
-
+    
     /**
      * Formats the date according to the <code>format_date</code> helper of the Date helper group.
      *
@@ -72,7 +74,7 @@ class CalendarDateManager extends CalendarTimeManager
     {
         return date('Y-m-d', $this->ts);
     }
-
+    
     /**
      * Formats the date according to the <code>format_datetime</code> helper of the Date helper group.
      *
@@ -82,7 +84,7 @@ class CalendarDateManager extends CalendarTimeManager
     {
         return date('Y-m-d H:i:s', $this->ts);
     }
-
+    
     /**
      * Format the date as a datetime value.
      *
@@ -92,18 +94,19 @@ class CalendarDateManager extends CalendarTimeManager
     {
         return date('Y-m-d H:i:s', $this->ts);
     }
-
+    
     /**
      * Retrieves the given unit of time from the timestamp.
      *
      * @param    int    unit of time (accepts sfTime constants).
+     *
      * @return    int    the unit of time
      *
      * @throws    sfDateTimeException
      */
     public function retrieve($unit = sfTime::DAY)
     {
-        switch ($unit) {
+        switch ( $unit ) {
             case CalendarTimeManager::SECOND:
                 return date('s', $this->ts);
             case CalendarTimeManager::MINUTE:
@@ -130,7 +133,7 @@ class CalendarDateManager extends CalendarTimeManager
                 throw new SS_HTTPResponse_Exception(sprintf('The unit of time provided is not valid: %s', $unit));
         }
     }
-
+    
     /**
      * Retrieve the timestamp value of this sfDate instance.
      *
@@ -140,7 +143,7 @@ class CalendarDateManager extends CalendarTimeManager
     {
         return $this->ts;
     }
-
+    
     /**
      * Sets the timestamp value of this sfDate instance.
      *
@@ -154,15 +157,15 @@ class CalendarDateManager extends CalendarTimeManager
     public function set($value = null)
     {
         $ts = CalendarManagerToolkit::getTS($value);
-
+        
         $this->ts = $ts;
         if ($this->init === null) {
             $this->init = $ts;
         }
-
+        
         return $this;
     }
-
+    
     /**
      * Resets the timestamp value of this sfDate instance to its original value.
      *
@@ -171,20 +174,21 @@ class CalendarDateManager extends CalendarTimeManager
     public function reset()
     {
         $this->ts = $this->init;
-
+        
         return $this;
     }
-
+    
     /**
      * Compares two date values.
      *
      * @param    mixed    timestamp, string, or sfDate object
+     *
      * @return    int        -1, 0, or 1
      */
     public function cmp($value)
     {
         $ts = CalendarManagerToolkit::getTS($value);
-
+        
         if ($this->ts < $ts) {
             // less than
             return -1;
@@ -192,24 +196,24 @@ class CalendarDateManager extends CalendarTimeManager
             // greater than
             return 1;
         }
-
+        
         // equal to
         return 0;
     }
-
+    
     /**
      * Gets the difference of two date values in seconds.
      *
-     * @param    mixed    timestamp, string, or sfDate object
+     * @param    mixed      timestamp, string, or sfDate object
      * @param    int        the difference in seconds
      */
     public function diff($value)
     {
         $ts = CalendarManagerToolkit::getTS($value);
-
+        
         return $this->ts - $ts;
     }
-
+    
     /**
      * Call any function available in the sfTime library, but without the ts parameter.
      *
@@ -226,173 +230,184 @@ class CalendarDateManager extends CalendarTimeManager
     public function __call($method, $arguments)
     {
         $callable = array('sfTime', $method);
-
+        
         if (!is_callable($callable)) {
             throw new sfDateTimeException(sprintf('Call to undefined function: %s::%s', 'sfDate', $method));
         }
-
+        
         array_unshift($arguments, $this->ts);
-
+        
         $this->ts = call_user_func_array($callable, $arguments);
-
+        
         return $this;
     }
-
-
+    
+    
     public static function isValid($dateString)
     {
         if (self::isDateValid($dateString)) {
             return date("Y-m-d", \strtotime($dateString));
         }
-
+        
         return "-";
     }
-
+    
     private static function isDateValid($dateString)
     {
         $intTime = \strtotime($dateString);
-
+        
         //Valid since PHP 5.1.0. Previous versions return -1 on failure.
-        return $intTime !== FALSE;
+        return $intTime !== false;
     }
-
+    
     static function currentTime()
     {
         return mktime();
     }
-
+    
     static function currentDateTime()
     {
         $intFromDateToTimeStamp = mktime();
+        
         return mktime(0, 0, 0, date('n', $intFromDateToTimeStamp), date('j', $intFromDateToTimeStamp), date('Y', $intFromDateToTimeStamp));
     }
-
+    
     static function phpTimeToMySQLDateTime($dtTime)
     {
         return date('Y-m-d H:i:s', $dtTime);
     }
-
+    
     static function phpTimeToMySQLTime($dtTime)
     {
         return date('Y-m-d', $dtTime);
     }
-
+    
     static function phpTimeToMySQLTimeQuoted($dtTime)
     {
         return "'" . self::phpTimeToMySQLTime($dtTime) . "'";
     }
-
+    
     static function calendarDateToPHPDate($strDate)
     {
         $iYear = (int)substr($strDate, 6, 4);
         $iMonth = (int)substr($strDate, 3, 2);
         $iDay = (int)substr($strDate, 0, 2);
+        
         return mktime(0, 0, 0, $iMonth, $iDay, $iYear);
     }
-
+    
     static function calendarDateToMySQLDate($strDate)
     {
         $dtDate = self::calendarDateToPHPDate($strDate);
+        
         return self::phpTimeToMySQLTime($dtDate);
     }
-
+    
     static function calendarDateToMySQLDateQuoted($strDate)
     {
         $dtDate = self::calendarDateToPHPDate($strDate);
+        
         return self::phpTimeToMySQLTimeQuoted($dtDate);
     }
-
+    
     static function addDaysToDate($dtDate, $iDays)
     {
         $arrDate = getdate($dtDate);
         $arrDate['mday'] += $iDays;
-
+        
         return mktime($arrDate['hours'], $arrDate['minutes'], $arrDate['seconds'], $arrDate['mon'], $arrDate['mday'], $arrDate['year']);
     }
-
+    
     static function mysqlDateToPHPDate($strDate)
     {
         $intFromDateToTimeStamp = strtotime($strDate);
+        
         return mktime(0, 0, 0, date('n', $intFromDateToTimeStamp), date('j', $intFromDateToTimeStamp), date('Y', $intFromDateToTimeStamp));
     }
-
+    
     static function mysqlDateToCalendarDate($strDate)
     {
         $dtTime = self::mysqlDateToPHPDate($strDate);
+        
         return date('d/m/Y', $dtTime);
     }
-
+    
     static function SSDateToBritishShortDate($strDate)
     {
         $dtTime = self::mysqlDateToPHPDate($strDate);
+        
         return date('d/m/y', $dtTime);
     }
-
+    
     static function mysqlDateToNiceDate($strDate)
     {
         $dtTime = self::mysqlDateToPHPDate($strDate);
+        
         return date('jS F Y H:ia', $dtTime);
     }
-
+    
     static function mysqlDateToNiceDateOnly($strDate)
     {
         $dtTime = self::mysqlDateToPHPDate($strDate);
+        
         return date('jS F Y', $dtTime);
     }
-
+    
     static function mysqlDateToNiceDateWithFullTextOnly($strDate)
     {
         $dtTime = self::mysqlDateToPHPDate($strDate);
+        
         return date('l jS F Y', $dtTime);
     }
-
+    
     static function phpTimeToNiceDateFormat($dtTime)
     {
         return date('l, j-m-Y', $dtTime);
     }
-
+    
     static function monthFromTime($dtTime)
     {
         return date("m", $dtTime);
     }
-
+    
     static function mysqlTimeToNiceTime($strTime)
     {
         $iLen = strlen($strTime);
         $strAMPM = strtoupper(substr($strTime, $iLen - 3));
         $strTime = substr($strTime, 0, -3);
-
+        
         if ($strAMPM == 'PM') {
             $arrParts = explode(':', $strTime);
             $iHour = (int)$arrParts[0];
             $iHour += 12;
             $strTime = $iHour . ':' . $arrParts[1];
         }
-
+        
         return $strTime;
     }
-
+    
     static function getSSTimeStr($dtDate)
     {
         return date('H:i:s', $dtDate);
     }
-
+    
     public static function getTimeStr($dtDate)
     {
         return date('H:i', $dtDate);
     }
-
+    
     public static function ssDateToPHPDate($strDate)
     {
         $intFromDateToTimeStamp = strtotime($strDate);
+        
         return mktime(0, 0, 0, date('n', $intFromDateToTimeStamp), date('j', $intFromDateToTimeStamp), date('Y', $intFromDateToTimeStamp));
     }
-
+    
     public static function getDayStr($dtDate)
     {
         return strftime('%A', $dtDate);
     }
-
+    
     static function geDateRange($start_date, $range = 10)
     {
         $begin = new DateTime($start_date);
@@ -402,48 +417,69 @@ class CalendarDateManager extends CalendarTimeManager
         $interval = new DateInterval('P1D');
         $dateRange = new DatePeriod($begin, $interval, $end_date);
         $days = array();
-        foreach ($dateRange as $date) {
+        foreach ( $dateRange as $date ) {
             $days[] = $date->format("Y-m-d");
         }
-
+        
         return $days;
     }
-
+    
     function isFirstDayOfTheMonth($date)
     {
         if (date('j', strtotime($date)) === '1') {
             return 'FirstDay';
         }
+        
         return false;
     }
-
+    
     static function isLastDayOfTheMonth($date)
     {
         $last_day = date('t', strtotime($date));
         if (date('j', strtotime($date)) === $last_day) {
             return 'LastDay';
         }
+        
         return false;
     }
-
+    
     static function isWeekend($date)
     {
         $date = date('D', strtotime($date));
         if (in_array($date, array("Sat", "Sun"))) {
             return 'weekend';
         }
+        
         return false;
     }
-
+    
     static function isDateOnWeekend($date)
     {
         return (date('N', strtotime($date)) >= 6);
     }
-
+    
     static function formatedDate($date)
     {
         $time = strtotime($date);
+        
         return sprintf("<span class='day'>%s</span><span class=\"date\">%s</span><span>%s</span>", date('D', $time), date('d', $time), date('M', $time));
     }
-
+    
+    /**
+     * @param $date
+     *
+     * @return bool
+     */
+    public static function InFuture($date)
+    {
+        $_date = new DateTime($date);
+        $today = new DateTime();
+        
+        if ($_date > $today) {
+            return true;
+        }
+        
+        return false;
+    }
+    
 }
