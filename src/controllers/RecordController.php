06a32e9bef52be4bba67ad5ff06057b504696367
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2018-11-26 15:26:10 +0200

    batch code update - common

--- src/controllers/RecordController.php
+++ src/controllers/RecordController.php
@@ -23,6 +23,14 @@ class RecordController extends InsiteMainController
         return "records/$action";
     }
     
+    public static function Guid()
+    {
+        mt_srand((double)microtime() * 10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        return substr($charid, 0, 8) . $hyphen . substr($charid, 8, 4) . $hyphen . substr($charid, 12, 4) . $hyphen . substr($charid, 16, 4) . $hyphen . substr($charid, 20, 12);
+    }
+    
     static function find_link( $action = false )
     {
         
