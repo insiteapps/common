0a1ffb22c4d039ab4700b1775fde819938ea7bbd
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-10-07 19:26:39 +0200

    batch code update - common

--- src/model/SecureDocumentLibrary.php
+++ src/model/SecureDocumentLibrary.php
@@ -6,39 +6,42 @@ use SilverStripe\ORM\DataObject;
 
 class SecureDocumentLibrary extends DataObject
 {
-
+    
     private static $default_sort = 'SortOrder';
-
+    
     private static $db = array(
         'Title'     => 'Varchar(255)',
         'Content'   => 'HTMLText',
         'SortOrder' => 'Int',
     );
-
+    
     private static $has_one = array(
         'File'           => 'File',
         'MemberAreaPage' => 'MemberAreaPage',
     );
-
+    
     public function getCMSFields()
     {
         $f = parent::getCMSFields();
-        $f->removeByName(['SortOrder', 'MemberAreaPage']);
-
+        $f->removeByName( [
+            'SortOrder',
+            'MemberAreaPage',
+        ] );
+        $f->addFieldToTab( 'Root.Main', HTMLEditorField::create( 'Content' )->setRows( 10 ) );
         return $f;
     }
-
+    
     private static $summary_fields = array(
         'Name',
         'Description',
     );
-
-    function getThumbnail()
+    
+    public function getThumbnail()
     {
         $image = $this->Image();
-        if ($image && $image->ID) {
+        if ( $image && $image->ID ) {
             return $image->CMSThumbnail();
         }
     }
-
+    
 }
