c6da6cc52b70cf7b7e8a176d1ea16a60b4b9e770
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-10-17 09:39:02 +0200

    batch code update - common

--- src/extensions/DataObjectURLSegmentExtension.php
+++ src/extensions/DataObjectURLSegmentExtension.php
@@ -11,15 +11,15 @@ use SilverStripe\Forms\FieldList;
 
 class DataObjectURLSegmentExtension extends DataExtension
 {
-
+    
     private static $db = array(
         'URLSegment' => 'Varchar(200)',
     );
-
+    
     private static $indexes = array(
         "URLSegment" => true,
     );
-
+    
     private static $casting = array(
         "Breadcrumbs"  => "HTMLText",
         "LastEdited"   => "SS_Datetime",
@@ -29,28 +29,28 @@ class DataObjectURLSegmentExtension extends DataExtension
         'AbsoluteLink' => 'Text',
         'TreeTitle'    => 'HTMLText',
     );
-
+    
     /**
      * @param \FieldList $fields
      */
-    public function updateCMSFields(FieldList $fields)
+    public function updateCMSFields( FieldList $fields )
     {
-        $fields->addFieldToTab("Root.Main", ReadonlyField::create("URLSegment"));
-
+        $fields->addFieldToTab( "Root.Main", ReadonlyField::create( "URLSegment" ) );
+        
     }
-
+    
     public function AbsoluteLink()
     {
-        return Director::absoluteURL($this->owner->Link());
+        return Director::absoluteURL( $this->owner->Link() );
     }
-
-
+    
+    
     public function MenuTitle()
     {
-        return $this->owner->getField("Title");
+        return $this->owner->getField( "Title" );
     }
-
-
+    
+    
     /**
      * Return the title, description, keywords and language metatags.
      *
@@ -60,68 +60,75 @@ class DataObjectURLSegmentExtension extends DataExtension
      *
      * @return string The XHTML metatags
      */
-    public function MetaTags($includeTitle = true)
+    public function MetaTags( $includeTitle = true )
     {
         $tags = "";
-        if ($includeTitle === true || $includeTitle == 'true') {
-            $tags .= "<title>" . Convert::raw2xml($this->owner->Title) . "</title>\n";
+        if ( $includeTitle === true || $includeTitle == 'true' ) {
+            $tags .= "<title>" . Convert::raw2xml( $this->owner->Title ) . "</title>\n";
         }
-
-        $generator = trim(Config::inst()->get('SiteTree', 'meta_generator'));
-        if (!empty($generator)) {
-            $tags .= "<meta name=\"generator\" content=\"" . Convert::raw2att($generator) . "\" />\n";
+        
+        $generator = trim( Config::inst()->get( 'SiteTree', 'meta_generator' ) );
+        if ( !empty( $generator ) ) {
+            $tags .= "<meta name=\"generator\" content=\"" . Convert::raw2att( $generator ) . "\" />\n";
         }
-
-        $charset = Config::inst()->get('ContentNegotiator', 'encoding');
-        $tags .= "<meta http-equiv=\"Content-type\" content=\"text/html; charset=$charset\" />\n";
-        if ($this->owner->MetaDescription) {
-            $tags .= "<meta name=\"description\" content=\"" . Convert::raw2att($this->owner->MetaDescription) . "\" />\n";
+        
+        $charset = Config::inst()->get( 'ContentNegotiator', 'encoding' );
+        $tags    .= "<meta http-equiv=\"Content-type\" content=\"text/html; charset=$charset\" />\n";
+        if ( $this->owner->MetaDescription ) {
+            $tags .= "<meta name=\"description\" content=\"" . Convert::raw2att( $this->owner->MetaDescription ) . "\" />\n";
         }
-        if ($this->owner->ExtraMeta) {
+        if ( $this->owner->ExtraMeta ) {
             $tags .= $this->owner->ExtraMeta . "\n";
         }
-
+        
         return $tags;
     }
-
-
+    
+    
     public function onBeforeWrite()
     {
+        $aTitle = [
+            $this->owner->Title,
+            $this->owner->Name,
+        ];
+        
+        $aTitle = array_filter( $aTitle );
+        
         // If there is no URLSegment set, generate one from Title
-        if ((!$this->owner->URLSegment && $this->owner->Title)) {
-            $this->owner->URLSegment = $this->generateUniqueURLSegment($this->owner->Title);
-        }
+        //if ((!$this->owner->URLSegment && $this->owner->Title)) {
+        $this->owner->URLSegment = $this->generateUniqueURLSegment( reset( $aTitle ) );
+        //}
         parent::onBeforeWrite();
     }
-
+    
     /*
 	* Generate Unique URLSegment
 	*/
-    public function generateUniqueURLSegment($title)
+    public function generateUniqueURLSegment( $title )
     {
-        $URLSegment = singleton('SiteTree')->generateURLSegment($title);
+        $URLSegment     = singleton( 'SiteTree' )->generateURLSegment( $title );
         $prevurlsegment = $URLSegment;
-        $i = 1;
-        while ( !$this->validURLSegment($URLSegment) ) {
+        $i              = 1;
+        while ( !$this->validURLSegment( $URLSegment ) ) {
             $URLSegment = $prevurlsegment . "-" . $i;
             $i++;
         }
-
+        
         return $URLSegment;
-
+        
     }
-
-    public function validURLSegment($URLSegment)
+    
+    public function validURLSegment( $URLSegment )
     {
-        $existingPage = $this->owner->get()->filter(array(
+        $existingPage = $this->owner->get()->filter( array(
             'URLSegment' => $URLSegment,
-        ))->exclude(array(
+        ) )->exclude( array(
             'ID' => $this->owner->ID,
-        ))->first();
-        if ($existingPage) {
+        ) )->first();
+        if ( $existingPage ) {
             return false;
         }
-
+        
         return true;
     }
 }
